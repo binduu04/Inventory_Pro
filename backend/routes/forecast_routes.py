7276dd1b89f4dbfdbff2406dbd04ff575208c646
AchhuthaGowda12 <agowda.n12@gmail.com>
2025-11-29 00:56:27 +0530

    crting wrong commit

--- backend/routes/forecast_routes.py
+++ backend/routes/forecast_routes.py
@@ -83,7 +83,7 @@ def generate_forecast_api():
         # Go up from backend/routes/ -> backend/ -> project_root/
         project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
         # csv_path = os.path.join(project_root, 'kirana_sales_data_v2.3_production_discount.csv')
-        csv_path = os.path.join(os.getcwd(), "kirana_sales_data_v2.3_production_discount.csv")
+        csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "kirana_sales_data_v2.3_production_discount.csv")
 
         if not os.path.exists(csv_path):
             return jsonify({
